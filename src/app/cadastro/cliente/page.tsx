92ae970ee23c458628dceaf8de73b7025b8fe828
Davi Costa <davim.costa@outlook.com>
2023-10-24 18:48:10 -0300

    Validação com Yup pt.1

--- src/app/cadastro/cliente/page.tsx
+++ src/app/cadastro/cliente/page.tsx
@@ -3,8 +3,9 @@ import Botao from '@/components/Botao'
 import CampoDigitacao from '@/components/CampoDigitacao'
 import Menu from '@/components/PaginaPadrao'
 import Titulo from '@/components/Titulo'
-import { useState } from 'react'
+import { FormEvent, useState } from 'react'
 import styled from 'styled-components'
+import * as Yup from 'yup';
 
 const FormEstilizado = styled.form`
     display: flex;
@@ -17,6 +18,25 @@ const DivEstilizada = styled.div`
     display: flex;
 `
 
+const Erro = styled.span`
+  font-size: 18px;
+  color: "#DA2A38";
+`
+
+const schema = Yup.object().shape({
+  cpf: Yup.string()
+    .matches(/^\d{3}\.\d{3}\.\d{3}-\d{2}$/, 'CPF inválido')
+    .required('CPF é obrigatório'),
+  email: Yup.string()
+    .email('Email inválido')
+    .required('Email é obrigatório'),
+  nome: Yup.string().required('Nome é obrigatório'),
+  telefone: Yup.string().required('Telefone é obrigatório'),
+  endereco: Yup.string().required('Endereço é obrigatório'),
+  nascimento: Yup.string().required('Nascimento é obrigatório'),
+});
+
+
 const Cliente = () => {
 
   const [email, setEmail] = useState('')
@@ -26,21 +46,47 @@ const Cliente = () => {
   const [endereco, setEndereco] = useState('')
   const [nascimento, setNascimento] = useState('')
 
-  function handleSubmit() {
-    console.log('Submit')
+  const [erros, setErros] = useState({
+    cpf: '',
+    email: '',
+    nome: '',
+    telefone: '',
+    endereco: '',
+    nascimento: '',
+  });
+  
+  function handleSubmit(event: FormEvent) {
+    event.preventDefault()
+    schema
+      .validate(
+        { cpf, email, nome, telefone, endereco, nascimento },
+        { abortEarly: false }
+      )
+      .then(() => {
+        console.log('Formulário válido, envie os dados');
+      })
+      .catch((error: Yup.ValidationError) => {
+        console.log(error)
+      });
   }
+  
+
 
   return (
     <Menu>
       <Titulo texto="Cadastro de cliente"  />
       <FormEstilizado onSubmit={handleSubmit}>
         <CampoDigitacao tipo="text" label="CPF" valor={cpf} placeholder="Insira seu CPF" onChange={setCPF} />
+        {erros.cpf && <Erro className="erro">{erros.cpf}</Erro>}
         <CampoDigitacao tipo="email" label="Email" valor={email} placeholder="Insira seu endereço de email" onChange={setEmail} />
+        {erros.email && <Erro className="erro">{erros.email}</Erro>} 
         <CampoDigitacao tipo="text" label="Nome" valor={nome} placeholder="Insira seu endereço de email" onChange={setNome} />
         <CampoDigitacao tipo="text" label="Telefone" valor={telefone} placeholder="Insira seu telefone" onChange={setTelefone} />
         <CampoDigitacao tipo="text" label="Endereço" valor={endereco} placeholder="Insira seu endereco" onChange={setEndereco} />
         <CampoDigitacao tipo="text" label="Nascimento" valor={nascimento} placeholder="Insira seu nascimento" onChange={setNascimento} />
-
+        {Object.values(erros).map((error, index) => (
+          <Erro key={index}>{error}</Erro>
+        ))}
 
       <DivEstilizada>
         <Botao texto='Confirmar' tipo='submit' />
