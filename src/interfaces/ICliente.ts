0f76a7e3bfd1f5de8792f535489f93af34d155b4
Davi Costa <davim.costa@outlook.com>
2023-10-28 17:41:05 -0300

    Método post

---
+++ src/interfaces/ICliente.ts
@@ -0,0 +1,9 @@
+
+
+export interface ICliente {
+    cpf: string,
+    nome: string,
+    telefone: string,
+    endereco: string,
+    nascimento: string
+}
---
+++ src/services/index.ts
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+export const http = axios.create({
+    baseURL: 'http://localhost:8000',
+    headers: {
+        Accept: 'application/json',
+        Content: 'application/json'
+    }
+})
