361fe7fb2d310e6cb59a0140a9251a8929d10e6d
kleuvyn <kleuvyn@hotmail.com>
2023-12-08 23:55:36 -0300

    atualizando

--- src/services/index.ts
+++ src/services/index.ts
@@ -4,14 +4,26 @@ export const http = axios.create({
     baseURL: 'http://localhost:3000',
     headers: {
         Accept: 'application/json',
-        Content: 'application/json'
+        'Content-Type': 'application/json'
     }
-})
+});
 
 export const httpTeste = axios.create({
     baseURL: 'http://localhost:3004',
     headers: {
         Accept: 'application/json',
-        Content: 'application/json'
+        'Content-Type': 'application/json'
     }
-})
+});
+
+http.interceptors.request.use((config) => {
+    const token = localStorage.getItem('token');
+   
+    if (config.url !== '/auth/login' && token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    
+    return config;
+}, (error) => {
+    return Promise.reject(error);
+});
